e6beab5c04afe14c94f6d9a35be5ae3098de0af9
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-06-05 16:19:13 -0500

    character amount function has been added

--- Develop/script.js
+++ Develop/script.js
@@ -9,9 +9,33 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  
+
+
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+  // check if the amount of charactors is correct
+  // random number
+  
+  //prompt user for # of characters
+
+var charactorNumber = function() {
+  var charactorNumberPrompt= window.prompt("Thank you for using this service.\nHow many characters should your password be?\n(Your password must be between 8 and 128 characters.)"); 
+  if (charactorNumberPrompt >= 8 && charactorNumberPrompt <= 128) {
+    //placeholder to check if working.
+    window.alert("Thank you!"); 
+    //move to function/prompt for special charactors question
+  }
+  else {
+    window.alert("Your response does not fit the criteria.");
+    //call charactor response number 
+    charactorNumber();
+  }
+}
+
+console.log(charactorNumber(7));
