09949758f612fa944f75b61872f21264f31e14f8
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-06-07 20:54:35 -0500

    worked on the password generating random of each type. Still not showing on screen

--- Develop/script.js
+++ Develop/script.js
@@ -1,129 +1,89 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+var charactersNeeded;
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  function writePassword() {
 
+    var characterNumberPrompt = window.prompt("Thank you for using this service.\nHow many characters should your password be?\n(Your password must be between 8 and 128 characters.)");
+    // check if the amount of characters is correct
+    if (characterNumberPrompt < 8 || characterNumberPrompt > 128) {
+      window.alert("Your response does not fit the criteria.");
+      writePassword();
+    }
+
+    neededChars();
+    function neededChars() {
+      charactersNeeded = {
+        passwordLength: characterNumberPrompt,
+        lowercase: window.confirm("Do you need to include lowercase letters?"),
+        uppercase: window.confirm("Do you need to include uppercase letters?"),
+        numeral: window.confirm("Do you need to include numerial?"),
+        special: window.confirm("Do you need to include special letters?")
+      }
+
+      if (charactersNeeded.lowercase === false && charactersNeeded.uppercase === false && charactersNeeded.numeral === false && charactersNeeded.special === false) {
+        window.alert("One of the four types of characters is needed to generate a password. Please try again.")
+        neededChars();
+      }
+    }
+
+    //Confirmation of different 
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    
+    function generatePassword() {
+      var passwordTemp = ""
+      var total = ""
+      console.log(charactersNeeded);
   
-
-
-  passwordText.value = password;
-
-}
+      if (charactersNeeded.lowercase) {
+        var letterL="abcefghijklmnopqrstuvwxyz"
+        var index = Math.floor(Math.random()*letterL.length)
+        passwordTemp = passwordTemp + letterL[index]
+        total = total + letterL
+  
+      };
+  
+      if (charactersNeeded.uppercase) {
+        var letterU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        var index = Math.floor(Math.random()*letterU.length)
+        passwordTemp = passwordTemp + letterU[index]
+        total = total + letterU
+      };
+  
+      if (charactersNeeded.numeral) {
+        var getNumeral = getRandomNumber()
+        function getRandomNumber() {
+          return Math.floor(Math.random() * 10);
+        }
+        passwordTemp = passwordTemp + getNumeral
+        total = total + "0123456789"
+      };
+  
+      if (charactersNeeded.special) {
+        var specialC = "!\"$%&/()=?";
+        var index = Math.floor(Math.random()*specialC.length)
+        passwordTemp = passwordTemp + specialC[index]
+        total = total + specialC
+      }
+    
+      console.log(passwordTemp, passwordTemp.length, total, charactersNeeded.passwordLength); 
+      lastChar();
+    
+      function lastChar() {        
+        while (passwordTemp.length < charactersNeeded.passwordLength) {
+            var index = Math.floor(Math.random()*total.length)
+            passwordTemp = passwordTemp + total[index]
+            console.log("Your password with the new letter is " + passwordTemp + " and your current password length is " + passwordTemp.length)
+        };
+      };
+      console.log(passwordTemp)
+      passwordTemp = password;
+    
+    };
+    return password;
+    
+  };
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-  // random number
-  
-  //prompt user for # of characters
-
-var charactorNumber = function() {
-  var charactorNumberPrompt= window.prompt("Thank you for using this service.\nHow many characters should your password be?\n(Your password must be between 8 and 128 characters.)"); 
-   // check if the amount of charactors is correct
-  if (charactorNumberPrompt >= 8 && charactorNumberPrompt <= 128) {
-    //placeholder to check if working.
-    window.alert("Thank you!"); 
-    //move to function/prompt for special charactors question
-  }
-  else {
-    window.alert("Your response does not fit the criteria.");
-    //call charactor response number 
-    charactorNumber();
-  }
-}
-
-
-var confirmLower = false;
-var confirmUpper;
-var confirmNumeric;
-var confirmSpecial;
-
-
-
-
-//
-fourTypes();
-//Confirmation of different 
-function fourTypes(windowConfirms) {
-  window.confirm("Do you need to include lowercase letters?")
-    if (true) {
-      confirmLower = true;
-    } 
-    console.log("The answer to lowercase is " + confirmLower)
-  window.confirm("Do you need to include uppercase letters?")
-    if (true) {
-      confirmUpper = true;
-    } else {
-    }
-    console.log("The answer to uppercase is " + confirmUpper)
-  window.confirm("Do you need to include numeric letters?")
-    if (true) {
-      confirmNumeric = true;
-    } else {
-    }
-    console.log("The answer to numeric is " + confirmNumeric)
-    window.confirm("Do you need to include special letters?")
-    if (true) {
-      confirmSpecial = true;
-    } else {
-    }
-    console.log("The answer to special is " + confirmSpecial)
-}
-// // prompt user for 4 different criteria
-// var characterTypes = [lowercase, uppercase, numeric, special]
-
-// //random generate lowercase
-// var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// var getLower = alpha[Math.floor(Math.random() * alpha.length) + 1];
-
-
-
-//       //String.fromCharCode(97+Math.floor(Math.random() * 26))
-
-// //random generate uppercase/lowercase
-var upperCase = getRandomNumber(65, 90)
-var lowerCase = getRandomNumber(97, 122)
-function getRandomNumber(min, max) { // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-console.log("This is a character " + String.fromCharCode(upperCase));
-console.log("This is a character " + String.fromCharCode(lowerCase));
-
-// random generate numeral
-function randomNumeral() {
-  for (var i = 0; i < 9; i++) {
-    var random = Math.floor(Math.random() * 9);
-    return ("Your random numeral is " + random)
-  }; 
-};
-console.log(randomNumeral())
-
-//random generate special character
-function randomSpecialChar () {
-  // example set of special chars as a string in no particular order
-  var s = "!#$%&'()*+,-./:;<=>?@][\"^_`{|}~";
-
-  // generating a random index into the string and extracting the character at that position
-  return s.substr(Math.floor(s.length*Math.random()), 1);
-} 
-
-//for loop?
-for (var i = 0; i < charactorNumber; i++) {
-
-}
-
-
-    // lowercase
-    // uppercase
-    // numeric
-    // special character
-
